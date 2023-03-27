d1ec4c3c334d15612f97c7629eba3f04003ad296
Adrian Zelada <zelada.torreza@gmail.com>
2023-03-26 23:31:17 -0400

    first commit

---
+++ src/main/java/com/sacavix/mq/publisher/Publisher.java
@@ -0,0 +1,30 @@
+package com.sacavix.mq.publisher;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+@EnableRabbit
+public class Publisher {
+	
+	 @Autowired
+	 private RabbitTemplate rabbitTemplate;
+
+     @Autowired
+     private Queue queue;
+     
+     public void send(Object message) {
+         rabbitTemplate.convertAndSend(queue.getName(), message);
+     }
+     public void received() {
+         Object data = rabbitTemplate.receiveAndConvert(queue.getName());
+         System.out.println(" data ");
+         System.out.println(data);
+     }
+
+}
